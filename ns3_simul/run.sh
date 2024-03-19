0f94013caa39101f790b58ea8a5686ecd50f678e
theblackoreo <fabio.tagliani03@universitadipavia.it>
2024-03-19 10:15:57 -0400

    udpate sh path

--- ns3_simul/run.sh
+++ ns3_simul/run.sh
@@ -6,7 +6,7 @@ git pull
 
 cp /home/fabiotagliani/Desktop/smesh/ns3_simul/test_ns3_wifi.cc /home/fabiotagliani/ns-allinone-3.33/ns-3.33/scratch/
 
-cd /home/fabiotagliani/Desktop/ns-allinone-3.33/ns-3.33/
+cd /home/fabiotagliani/ns-allinone-3.33/ns-3.33/
 
 ./waf --run scratch/test_ns3_wifi.cc
 
