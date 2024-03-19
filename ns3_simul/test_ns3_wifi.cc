9216e52417a2cdec22b880ca0bf6a998b7f10171
theblackoreo <fabio.tagliani03@universitadipavia.it>
2024-03-19 10:03:33 -0400

    update wifi -> H

--- ns3_simul/test_ns3_wifi.cc
+++ ns3_simul/test_ns3_wifi.cc
@@ -82,7 +82,7 @@ int main (int argc, char *argv[])
 
   // Create wifi
   WifiHelper wifi;
-  wifi.SetStandard(WIFI_STANDARD_80211n);
+  wifi.SetStandard(WIFI_STANDARD_H);
 
   WifiMacHelper mac;
   mac.SetType ("ns3::AdhocWifiMac");
