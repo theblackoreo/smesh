afe3f4fba389dd03c9abb71d6b0f98f47759d5c0
theblackoreo <fabio.tagliani03@universitadipavia.it>
2024-03-19 10:12:57 -0400

    sh update

--- ns3_simul/run.sh
+++ ns3_simul/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-cp /Users/fabiotagliani/Desktop/smesh/ns3_simul/test_ns3_wifi.cc /Users/fabiotagliani/Desktop/workspace/ns-3-dev/scratch/
 
-cd /Users/fabiotagliani/Desktop/workspace/ns-3-dev/
+cd /home/fabiotagliani/Desktop/smesh
+git pull
 
-./ns3 build
+cp /home/fabiotagliani/Desktop/smesh/ns3_simul/test_ns3_wifi.cc /home/fabiotagliani/Desktop/ns-allinone-3.33/ns-3.33/scratch/
 
-./ns3 run scratch/test_ns3_wifi.cc --vis
+cd /home/fabiotagliani/Desktop/ns-allinone-3.33/ns-3.33/
+
+./waf --run scratch/test_ns3_wifi.cc
+
 
